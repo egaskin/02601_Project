970ce5b607f2a3870e2762ca3ea42fd477a652c1
Akshat Gupta <63586079+Officialbrute@users.noreply.github.com>
2022-12-05 13:40:58 -0500

    Update akshat_functons.go

--- akshat_functons.go
+++ akshat_functons.go
@@ -3,11 +3,16 @@ package main
 import "math/rand"
 
 //UpdateAge takes in a pointer to a prey object, updates it age by incrementing it by one and then returns it.
-func UpdateAge(p *Prey) {
+func UpdateAgePrey(p *Prey) {
 	p.Organism.age += 1
 }
 
-func Reproduce(p *Prey) *Prey {
+//UpdateAge takes in a pointer to a prey object, updates it age by incrementing it by one and then returns it.
+func UpdateAgePredator(p *Predator) {
+	p.Organism.age += 1
+}
+
+func ReproducePrey(p *Prey) *Prey {
 	//This function will only be called if the age and energy and requirements are met. Check these requirements before calling this function.
 	var child Prey
 	p.Organism.age = 0
@@ -47,4 +52,16 @@ func UpdateGenome(currentOrganism *Organism) {
 			}
 		}
 	}
+}
+
+func ReproducePredator(p *Predator) *Predator {
+	//This function will only be called if the age and energy and requirements are met. Check these requirements before calling this function.
+	var child Predator
+	p.Organism.age = 0
+	child.Organism.energy = p.Organism.energy / 2
+	p.Organism.energy /= 2
+	child.Organism.genome = p.Organism.genome // Check if the array needs to be copied manually.
+	UpdateDirection(&p.Organism, &child.Organism)
+	UpdateGenome(&child.Organism)
+	return &child
 }
